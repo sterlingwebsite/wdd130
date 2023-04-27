b2b2c218eb308dd94cd169f91bf081bf772b86a6
sterlingwebsite <smsteele14@gmail.com>
2023-04-27 12:14:43 -0600

    added python assignment requirements file. week 2

---
+++ wwr/W02Assignment-SitePlanningPublish.py
@@ -0,0 +1,42 @@
+# Sterling Steele
+# WDD 130
+# W02 Assignment: Site Planning
+
+# Assignment Requirements
+# Complete Creating a Site Plan
+# Submit the URL of the site-plan-rafting.html
+# W02: Site Planning Rubric
+
+# Complete Creating a Site Plan
+# 01 Prepare a place for our site plan. Done.
+# 02 Make Additions and/or Changes to the Template. Done.
+# 03 Purpose Statement. Done.
+# 04 Audience Statement. Done.
+# 05 Choose a logo. Done.
+# 06 Color Palette. Done.
+# 07 Using Coolors.co. Done.
+# 08 Record the color codes. Done.
+# 09 Save your color scheme. Done.
+# 10 About Fonts. Done.
+# 11 Choose your fonts. Done.
+# 12 Color your fonts. Done.
+# 13 Style your navigation. Done.
+# 14 Site Map & Wireframes. Done.
+# 15 Submit to Canvas. Done.
+
+
+# Submit the URL of the site-plan-rafting.html
+#   https://sterlingwebsite.github.io/wdd130/wwr/site-plan-rafting.html Done.
+
+# W02: Site Planning Rubric
+# Criteria
+# All of the [square bracket] sections were replaced with actual content.
+#   The [square bracket] sections mentioned in the assignment should be replaced with individual student content. This includes: Site Name, Student Name, Student Section Number, Purpose, Audience, Hex numbers. Done.
+# Purpose Statement and Target Audience Statement.
+#   Purpose Statement explaining the purpose of the White Water Rafting website and what the website will accomplish. Target Audience Statement describing the audience of the website is included. Done.
+# Color Scheme and Site Plan documentation
+#   Color numbers in the HTML AND CSS are changed from the default colors in the site plan. This includes the HTML in the Palette Section of the site plan AND the CSS in the Palette section as well as font colors. Colors should be compatible. Evidence of compatible colors is provided (such as coolors link as described in the lesson material). Done.
+# Fonts chosen and indicated in the site plan.
+#   HTML and CSS changed to reflect new fonts. If using Google fonts, the fonts are embedded in the first line of the CSS stylesheet using @import. Done.
+# Navigation bar
+#   Navigation colors are changed from default and updated using colors from the chosen color palette . The updated colors include colors for links, background color, and colors on hover. Done.
